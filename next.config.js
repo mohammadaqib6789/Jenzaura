0fdb405c4c7948f8dcc0f321588bcdd58065dde1
mohammadaqib6789 <mohammadaqib6789@gmail.com>
2025-09-25 12:48:41 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true, // optional but recommended for faster builds
-}
-
-module.exports = nextConfig
