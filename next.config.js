6addfe50d727bc5bd20d6e7fad7e1e010a341d2f
mohammadaqib6789 <mohammadaqib6789@gmail.com>
2025-09-25 11:58:06 +0530

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true, // optional but recommended for faster builds
+}
+
+module.exports = nextConfig
